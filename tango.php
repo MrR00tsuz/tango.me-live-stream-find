89105659f2fd5b81fa99d8a456063dc86ed29a33
Bay Köksüz <93054788+MrR00tsuz@users.noreply.github.com>
2025-03-28 13:29:45 +0300

    tango.php

---
+++ tango.php
@@ -0,0 +1,89 @@
+<?php
+function getTangoStreamInfo($streamId) {
+    $url = "https://gateway.lemonshcherbet.com/stream/social/v1/{$streamId}/info";
+    
+    $params = [
+        "includeStream" => "false",
+        "includeAnchor" => "false",
+        "includeAnchorPoints" => "false",
+        "includeViewerCount" => "false",
+        "includeLikeCount" => "false",
+        "includePostId" => "false",
+        "includeTotalPointsInStream" => "false",
+        "includeUniqueViewerCount" => "false",
+        "includeBlps" => "false",
+        "includeStickers" => "true"
+    ];
+
+    $queryString = http_build_query($params);
+    $fullUrl = "{$url}?{$queryString}";
+
+    $headers = [
+        "Accept-Encoding: gzip",
+        "Authorization: Bearer YOUR",
+        "Connection: Keep-Alive",
+        "foreground_id: YOUR",
+        "Host: gateway.lemonshcherbet.com",
+        "interaction_id: YOUR",
+        "tg-carrier-country: cn",
+        "tg-loc-country: TR",
+        "tg-loc-language: tr",
+        "tg-vpn: 1",
+        "User-Agent: YOUR",
+        "X-APP-AB: v=1;proxy=0;referrer=webgoodeta.com;pcidss=0",
+        "X-App-Client-Session-Id: YOUR"
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $fullUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_ENCODING, "gzip");
+
+    $response = curl_exec($ch);
+    
+    if (curl_errno($ch)) {
+        return ['error' => true, 'message' => "cURL Error: ".curl_error($ch)];
+    }
+
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if ($httpCode != 200) {
+        return ['error' => true, 'message' => "HTTP Error: $httpCode"];
+    }
+
+
+    $hexResponse = bin2hex($response);
+    $urlPattern = '68747470733a2f2f63696e656d612d'; 
+    $urls = [];
+    $position = 0;
+
+    while (($pos = strpos($hexResponse, $urlPattern, $position)) !== false) {
+        $urlHex = substr($hexResponse, $pos, 600); 
+        $url = hex2bin($urlHex);
+        
+        preg_match('/https:\/\/[^\s\x00-\x1F]+/i', $url, $matches);
+        if (!empty($matches[0])) {
+            $cleanUrl = filter_var($matches[0], FILTER_SANITIZE_URL);
+            $urls[] = $cleanUrl;
+        }
+        $position = $pos + 1;
+    }
+
+    $urls = array_unique($urls);
+    return ['error' => !count($urls), 'urls' => array_values($urls)];
+}
+
+header('Content-Type: application/json');
+
+if (isset($_GET['streamid'])) {
+    $result = getTangoStreamInfo($_GET['streamid']);
+    echo json_encode($result, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+} else {
+    echo json_encode(
+        ['error' => true, 'message' => 'Missing streamid parameter'],
+        JSON_PRETTY_PRINT
+    );
+}
+?>
